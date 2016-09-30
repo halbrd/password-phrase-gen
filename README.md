e31cf06e3661a829ebd32d7c1823498f1d4ad490
William Holt <halbrd@gmail.com>
2016-10-01 00:11:05 +1000

    Add upper limit parameter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # password-phrase-gen
 A Python script to generate random phrases for passwords
+
+`MIN_WORD_LENGTH`, `MAX_WORD_LENGTH`, and `PHRASE_LENGTH` can be modified at the top of the script.
--- phrasegen.py
+++ phrasegen.py
@@ -1,6 +1,7 @@
 import random
 
 MIN_WORD_LENGTH = 8
+MAX_WORD_LENGTH = 99
 PHRASE_LENGTH = 4
 
 print("Reading words...")
@@ -9,9 +10,13 @@ with open("corncob_lowercase.txt", "r") as corncob:
 totalWordCount = len(words)
 print("Read", totalWordCount, "words.")
 
-print("Deleting words shorter than", str(MIN_WORD_LENGTH) + "...")
-words = [word for word in words if len(word) >= MIN_WORD_LENGTH]
+print("Deleting words shorter than", str(MIN_WORD_LENGTH), "or longer than", str(MAX_WORD_LENGTH) + "...")
+words = [word for word in words if len(word) >= MIN_WORD_LENGTH and len(word) <= MAX_WORD_LENGTH]
 print("Deleted", totalWordCount - len(words), "words (word count is now", str(len(words)) + ").\n")
+
+if len(words) < 1:
+	print("No words matched the given conditions.")
+	exit()
 
 while True:
 	for _ in range(PHRASE_LENGTH):
